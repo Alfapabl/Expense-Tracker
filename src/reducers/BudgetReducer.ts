49fca30be7cc0e8a1fe1cd5149aa8196f6f2172f
Pablo Mendez <Pablo-mendez09@hotmail.com>
2025-04-23 02:49:10 -0600

    fixed reset reducer not returning filter ID

--- src/reducers/BudgetReducer.ts
+++ src/reducers/BudgetReducer.ts
@@ -1,5 +1,5 @@
 import { expenseDraft, expenses } from "../types"
-import {v4 as uuidv4} from 'uuid'
+import { v4 as uuidv4 } from 'uuid'
 
 export type budgetType = {
     budget: number
@@ -7,18 +7,18 @@ export type budgetType = {
     expense: expenses[]
     editID: string
     filterID: string
-    
+
 }
 
 export type budgetAction =
     { type: 'add-budget', payload: { budget: number } } |
     { type: 'open-modal' } |
     { type: 'close-modal' } |
-    { type: 'add-expense', payload: {formExpenses: expenseDraft}} |
-    { type: 'remove-expense', payload: {id: expenses['id']}} |
-    { type: 'edit-expense', payload: {id: expenses['id']}} |
-    { type: 'reset'} |
-    { type: 'filterID', payload: {id: expenses['id']}}
+    { type: 'add-expense', payload: { formExpenses: expenseDraft } } |
+    { type: 'remove-expense', payload: { id: expenses['id'] } } |
+    { type: 'edit-expense', payload: { id: expenses['id'] } } |
+    { type: 'reset' } |
+    { type: 'filterID', payload: { id: expenses['id'] } }
 
 
 export const initialValue: budgetType = {
@@ -29,11 +29,11 @@ export const initialValue: budgetType = {
     filterID: ''
 }
 
-const createExpense = (expenseDraft: expenseDraft) : expenses => {
-   return {
-    ...expenseDraft,
-    id: uuidv4()
-   }
+const createExpense = (expenseDraft: expenseDraft): expenses => {
+    return {
+        ...expenseDraft,
+        id: uuidv4()
+    }
 
 }
 
@@ -41,7 +41,7 @@ export const budgetReducer = (
     state: budgetType = initialValue,
     action: budgetAction
 
-)=> {
+) => {
 
     switch (action.type) {
         case 'add-budget':
@@ -63,45 +63,51 @@ export const budgetReducer = (
                 editID: ''
             }
 
-        case 'add-expense' :
+        case 'add-expense':
             const expenses = createExpense(action.payload.formExpenses)
-            let editExpenses: expenseDraft[] = []
+            let editExpenses: expenses[] = []
 
             if (state.editID) {
-                editExpenses = state.expense.map((expense)=> state.editID === expense.id ?  action.payload.formExpenses : expense)
+                editExpenses = state.expense.map((expense) =>
+                    state.editID === expense.id
+                      ? { ...action.payload.formExpenses, id: expense.id } // ✅ retain ID
+                      : expense
+                  );
+                  
             }
             else {
-                editExpenses =  [...state.expense , expenses]
+                editExpenses = [...state.expense, expenses]
             }
-            
+
             return {
                 ...state,
                 expense: editExpenses,
                 editID: ''
             }
-        
-        case 'remove-expense' :
+
+        case 'remove-expense':
             return {
                 ...state,
-                expense: state.expense.filter((expense)=> expense.id !== action.payload.id)
+                expense: state.expense.filter((expense) => expense.id !== action.payload.id)
             }
 
-        case 'edit-expense' :
+        case 'edit-expense':
             return {
                 ...state,
                 modal: true,
-                editID: action.payload.id               
+                editID: action.payload.id
             }
 
-        case 'reset' :
+        case 'reset':
             return {
                 budget: 0,
                 modal: false,
                 expense: [],
-                editID: ''
+                editID: '',
+                filterID: ''
             }
 
-        case 'filterID' :
+        case 'filterID':
             return {
                 ...state,
                 filterID: action.payload.id
@@ -109,7 +115,7 @@ export const budgetReducer = (
 
 
         default:
-            return state
+            return state 
     }
 
 }
